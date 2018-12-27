e050e06c84c94e7d1a8fa2d1d23a1a4451b1a813
minjun kim <rlaalswns523@gmail.com>
2018-12-27 18:21:52 +0900

    html id속성을 이용한 css 적용 테스트

--- src/containers/Next.js
+++ src/containers/Next.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
+import './Next.css';
 
-const propTypes = {
+const propTypes = {};
 
-};
+const defaultProps = {};
 
-const defaultProps = {
-
-};
-
-class Next extends Component { 
-
+class Next extends Component {
     render() {
-        return(
-            <div>Next</div>
+        return (
+            <div id="Next">
+                <div className="Next1">Next1</div>
+                <div className="Next2">Next2</div>
+                <div className="Next3">Next3</div>
+            </div>
         );
     }
 }
 
 Next.propTypes = propTypes;
 
-Next.defaultProps = defaultProps
+Next.defaultProps = defaultProps;
 
 export default Next;
